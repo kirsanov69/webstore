f5f9231da2cf9ee42003ae8dfc428013abe06df8
kirsanov69 <kirsanov118@gmail.com>
2024-09-03 08:17:02 +0400

    product filter on the site page & urlencode

--- goods/views.py
+++ goods/views.py
@@ -6,15 +6,25 @@ from goods.models import Product
 
 # Create your views here.
 def catalog(request, category_slug):
+
+
     page = request.GET.get('page',1)
+    on_sale = request.GET.get('on_sale',None)
+    order_by = request.GET.get('order_by',None)
+
+
     if category_slug == 'all':
         goods = Product.objects.all()
 
     else:
         goods = get_list_or_404(Product.objects.filter(category__slug=category_slug))
-        # if not goods.exists():
-        #     raise Http404(f'Категория "{category_slug}" не найдена')
-        
+
+    if on_sale:
+        goods = goods.filter(discount__gt=0)
+
+    if order_by and order_by !='default':
+        goods = goods.order_by(order_by)
+
     paginator = Paginator(goods, 3)
     current_page = paginator.page(int(page))
 
