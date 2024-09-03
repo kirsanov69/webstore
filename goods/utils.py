7184baf7cb1a7c96f69f9fc3ad0f393c157d6f50
kirsanov69 <kirsanov118@gmail.com>
2024-09-03 16:49:22 +0400

    full text search - done

--- goods/utils.py
+++ goods/utils.py
@@ -1,5 +1,10 @@
 from django.db.models import Q
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import (
+    SearchVector,
+    SearchQuery,
+    SearchRank,
+    SearchHeadline,
+)
 
 
 from goods.models import Product
@@ -8,14 +13,34 @@ from goods.models import Product
 def q_search(query):
     if query.isdigit() and len(query) <= 5:
         return Product.objects.filter(id=int(query))
-    
-    vector = SearchVector('name', 'description')
-    query = SearchQuery(query)
-    
-    return Product.objects.annotate(
-        rank = SearchRank(vector, query)).order_by('-rank')  
 
-    
+    vector = SearchVector("name", "description")
+    query = SearchQuery(query)
+
+    result = (
+        Product.objects.annotate(rank=SearchRank(vector, query))
+        .filter(rank__gt=0)
+        .order_by("-rank")
+    )
+
+    result = result.annotate(
+        headline=SearchHeadline(
+            "name",
+            query,
+            start_sel='<span style="background-color: yellow;">',
+            stop_sel="</span>",
+        )
+    )
+    result = result.annotate(
+        bodyline=SearchHeadline(
+            "description",
+            query,
+            start_sel='<span style="background-color: yellow;">',
+            stop_sel="</span>",
+        )
+    )
+
+    return result
     # keywords = [word for word in query.split() if len(word) > 2]
 
     # q_objects = Q()
