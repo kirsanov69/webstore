65ea5bbcb2330d594d3c419bcf0ef110c58e8d4e
kirsanov69 <kirsanov118@gmail.com>
2024-09-08 11:51:55 +0400

    login required decorator - redirect to target page

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,7 @@
+from email.mime import image
 from pyexpat import model
 from django import  forms
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.forms import fields
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 
 from users.models import User
 
@@ -50,3 +50,61 @@ class UserRegistrationForm(UserCreationForm):
         password1 = forms.CharField()
         password2 = forms.CharField()
 
+
+class ProfileForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = (
+            'image',
+            'first_name',
+            'last_name',
+            'username',                
+            'email',            
+        )
+        image = forms.ImageField(required=False)
+        first_name = forms.CharField()
+        last_name = forms.CharField()
+        username = forms.CharField()
+        email = forms.EmailField()
+
+
+        # image = forms.ImageField(
+        #     widget=forms.FileInput(attrs={"class":"form-control mt-3"}), required=False
+        # )
+
+        # first_name = forms.CharField(
+        #     widget=forms.TextInput(
+        #         attrs={
+        #             "class":"form-control",
+        #             "placeholder":"ВВедите ваше имя"
+        #             }
+        #             )
+        # )
+
+        # last_name = forms.CharField(
+        #     widget=forms.TextInput(
+        #         attrs={
+        #             "class":"form-control",
+        #             "placeholder":"Введите вашу фамилию"
+        #             }
+        #             )
+        # )
+
+        # username = forms.CharField(
+        #     widget=forms.TextInput(
+        #         attrs={
+        #             "class":"form-control",
+        #             "placeholder":"Введите ваш логин"
+        #             }
+        #             )
+        # )
+
+        # email = forms.EmailField(
+        #     widget=forms.EmailInput(
+        #         attrs={
+        #             "class":"form-control",
+        #             "placeholder":"Введите ваш email"
+        #             }
+        #             )
+        # )
+    
